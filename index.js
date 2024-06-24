2cc7949d13a857dff4adf9f472ee63aa91be70bb
rencsikmarian <mariann4you@gmail.com>
2024-06-24 21:01:23 +0000

    Upload core build artifacts

---
+++ index.js
@@ -0,0 +1,13 @@
+// Platforms Map
+export { CapacitorPlatforms, addPlatform, setPlatform } from './platforms';
+// Global APIs
+export { Capacitor, registerPlugin } from './global';
+// Base WebPlugin
+export { WebPlugin } from './web-plugin';
+// Core Plugins APIs
+export { CapacitorCookies, CapacitorHttp, WebView, buildRequestInit, } from './core-plugins';
+// Constants
+export { CapacitorException, ExceptionCode } from './util';
+// Legacy Global APIs
+export { Plugins, registerWebPlugin } from './global';
+//# sourceMappingURL=index.js.map
---
+++ legacy/legacy-definitions.js
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=legacy-definitions.js.map
